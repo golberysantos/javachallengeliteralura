06bd1f4d6372925c016226fc95312005c6fca349
Golbery <golbery.santos@yahoo.com.br>
2024-07-09 14:58:45 -0400

    configurações e atualizações de dependências

--- literalura/src/main/java/br/com/literalura/principal/LiteraluraApplication.java
+++ literalura/src/main/java/br/com/literalura/principal/LiteraluraApplication.java
@@ -2,8 +2,9 @@ package br.com.literalura.principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
